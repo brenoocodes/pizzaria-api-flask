6e140aba8f1d3153fc952470fe9a47439cea9dbd
Breno Carvalho <brenos200304@gmail.com>
2024-02-17 12:30:46 -0300

    finalizando configurações locais

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
+
 
 # Inicialização do aplicativo Flask
 app = Flask(__name__)
-
+load_dotenv()
 # Configuração da chave secreta para proteger sessões e outros dados
-app.config['SECRET_KEY'] = 'FsjdejefweFRFWG#3452%@%@TRWWewrgwg4rtwghyettwwt254536g'
+app.config['SECRET_KEY'] = os.getenv("SECRETY_KEY")
 
 # Configuração da URI do banco de dados SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pizzaria-api-brenocodes@34.70.227.254/pizzaria'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 # Inicialização do objeto de banco de dados SQLAlchemy
 db = SQLAlchemy(app)
